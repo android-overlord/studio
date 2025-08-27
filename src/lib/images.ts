f8212611fea23eee65309c1ce8b1e399b55dff9b
Adarsh <sahoo.adarsh@gmail.com>
2025-08-27 05:18:40 +0000

    how about remove all signs of the qr and all old redundant code thats no

--- src/lib/images.ts
+++
@@ -1,5 +0,0 @@
-// Generate 312 image paths pointing to the public/images directory.
-// This assumes the images are named 1.jpg, 2.jpg, 3.jpg, and so on.
-export const images = Array.from(
-  { length: 312 }, (_, i) => `/images/extracted-image-${i + 1}.png`
-);
