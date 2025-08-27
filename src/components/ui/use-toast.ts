49fedd6165f7db823b3822b5f83297a54e060226
Adarsh <sahoo.adarsh@gmail.com>
2025-08-27 07:07:19 +0000

    The Netlify deploy errored, with the following guidance provided:
    ### D

--- src/components/ui/use-toast.ts
+++ src/components/ui/use-toast.ts
@@ -1,11 +1,13 @@
 // Inspired by react-hot-toast library
 import * as React from "react"
 
+import type { ToastActionElement, ToastProps } from "@/components/ui/toast"
+
 type ToasterToast = ToastProps & {
   id: string
   title?: React.ReactNode
   description?: React.ReactNode
-  action?: React.ReactElement<typeof ToastAction>
+  action?: ToastActionElement
 }
 
 const TOAST_LIMIT = 1
